0d57eae21b28cf17d693b3ef65976237f6d2d9b7
And1249 <146764275+And1249@users.noreply.github.com>
2024-01-29 10:50:44 -0600

    Update streamlit_app.py
    Se agregaron 4 campos nuevos.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healty Diner')
+
+streamlit.header('Menú de desayuno')
+streamlit.text('Omega 3 y avena con arándanos')
+streamlit.text('Batido de col rizada, espinacas y rúcula')
+streamlit.text('Huevo duro de gallinas camperas')
