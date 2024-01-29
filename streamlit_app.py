171901f9ba767291da37aac8cbbd518449810ddc
And1249 <146764275+And1249@users.noreply.github.com>
2024-01-29 11:02:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('🐔 Huevo duro de gallinas camperas')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
