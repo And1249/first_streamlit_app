4ffc3c88ce49307964f266adbbd1cc3383226bf0
And1249 <146764275+And1249@users.noreply.github.com>
2024-01-29 10:55:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Menú de desayuno')
-streamlit.text('Omega 3 y avena con arándanos')
-streamlit.text('Batido de col rizada, espinacas y rúcula')
-streamlit.text('Huevo duro de gallinas camperas')
+streamlit.text('🥣 Omega 3 y avena con arándanos')
+streamlit.text('🥗 Batido de col rizada, espinacas y rúcula')
+streamlit.text('🐔 Huevo duro de gallinas camperas')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
