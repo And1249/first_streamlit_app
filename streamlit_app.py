71143cbb466805abc4b8120b504b2db78e836d42
And1249 <146764275+And1249@users.noreply.github.com>
2024-01-29 11:05:21 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,8 @@ streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+# Pongamos una lista de selección aquí para que puedan elegir la fruta que quieran incluir 
+streamlit.multiselect("Elige algunas frutas:", list(my_fruit_list.index)) 
+
+# Muestra la tabla en la página.
